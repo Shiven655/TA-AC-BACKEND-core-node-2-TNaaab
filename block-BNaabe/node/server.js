4aacf9c281422ed9f1540369f37e196780de0105
Shiven655 <kshiven655@gmail.com>
2022-03-25 09:08:18 +0530

    block-BNaabe

--- block-BNaabe/node/server.js
+++ block-BNaabe/node/server.js
@@ -1,11 +1,31 @@
-let http = require('http');
-let qs = require('querystring');
+//Path
 let path = require('path');
-let fs = require('fs');
-/*
-//Path 
 console.log(__filename);
 console.log(__dirname + 'app.js');
-console.log(./index.html);
-path.join(__dirname, 'index.html');
-*/
+console.log('./index.html');
+var indexPath = path.join(__dirname, 'index.html');
+
+let qs = require('querystring');
+
+let http = require('http');
+const { isGeneratorFunction } = require('util/types');
+
+var server = http.createServer(handleRequest);
+function handleRequest(req, res) {
+  if (req.method === 'POST' && req.url === '/') {
+    var store = '';
+    req
+      .on('data', (chunk) => {
+        store += chunk;
+      })
+      .on('end', () => {
+        res.statusCode = 201;
+        var parsedData = qs.parse(store);
+        res.end(JSON.stringify(parsedData));
+      });
+  }
+}
+
+server.listen(5000, () => {
+  console.log('server is listning on 5k');
+});
